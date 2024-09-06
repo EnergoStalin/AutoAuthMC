603db345f4d745b54e5b6aa8a1558c24812e875d
EnergoStalin <ens.stalin@gmail.com>
2024-09-06 11:18:50 +0300

    chore: -ServerRecordAdapter

--- src/main/java/ru/energostalin/autoauth/lib/storages/MutableStorage.kt
+++ src/main/java/ru/energostalin/autoauth/lib/storages/MutableStorage.kt
@@ -1,10 +1,5 @@
 package ru.energostalin.autoauth.lib.storages
 
 interface MutableStorage : Storage {
-    interface ServerRecordAdapter {
-        fun save(record: Storage.ServerRecord): Storage.ServerRecord
-        fun load(record: Storage.ServerRecord?): Storage.ServerRecord?
-    }
-
     fun addOne(record: Storage.ServerRecord)
 }
